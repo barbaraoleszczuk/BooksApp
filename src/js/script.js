e84b0dce23887d9ff9c51c2b21f449049155c03e
Barbara Oleszczuk <basiaoleee@o2.pl>
2023-03-28 19:28:19 +0200

    Add raiting and background

--- src/js/script.js
+++ src/js/script.js
@@ -23,25 +23,30 @@
     
     
   function renderBookList(){
-    const thisBook = this;
+    
+    for (let book of dataSource.books){
 
-    for (const book of dataSource.books){
+      const ratingBgc = determineRatingBgc(book.rating);// stała, która zwraca raiting-ocenę
+      const ratingWidth = 10 * ratingBgc;// szerokość paska
+      book.ratingBgc = ratingBgc;
+      book.ratingWidth = ratingWidth;
+
       const generatedHTML = templates.bookList(book); // html dla każdej książki(zwykły string)
       //console.log('bookhtml',generatedHTML);
       //Kod HTML to jednak zwykły tekst, a my potrzebujemy elementu DOM, który będziemy w stanie naprawdę "wcisnąć" gdzieś na naszą stronę. Najlepiej zapamiętaj to tak: HTML to zwykły string, a element DOM to obiekt wygenerowany przez przeglądarkę na podstawie kodu HTML. Obiekt, który ma właściwości (np. innerHTML czy metody (np. getAttribute).
-      //JS nie ma wbudowanej metody, która służy do tego celu – dlatego skorzystamy z jednej z funkcji zawartych w obiekcie utils. Przygotowaliśmy go dla Ciebie, aby usprawnić nam pracę nad tym projektem. W tym wypadku użyjemy metody utils.createDOMFromHTML. 
+      //JS nie ma wbudowanej metody, która służy do tego celu – dlatego skorzystamy z jednej z funkcji zawartych w obiekcie utils.  metoda utils.createDOMFromHTML. 
         
       /*create element using utils.createElementFromHTML*/
-      thisBook.element = utils.createDOMFromHTML(generatedHTML);
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
       /*find container of book list*/
       
       const booksContainer = document.querySelector(select.containerOf.booksList);
       //console.log('container bookList',booksContainer);
       /*add element book to container of books List */
-      booksContainer.appendChild(thisBook.element);
+      booksContainer.appendChild(generatedDOM);
     }
   }
-  
+ 
 
   const favoriteBooks = [];
   const filters = [];
@@ -103,7 +108,17 @@
       }
     }
   }
+  function determineRatingBgc(rating){
+    let ratingBgc = '';
+
+    if (rating < 6) ratingBgc = {background : ' linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)'};
+    else if (rating > 6 && rating <= 8) ratingBgc = {background :' linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)'};
+    else if (rating > 8 && rating <= 9) ratingBgc = {background : ' linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)'};
+    else if (rating > 9) ratingBgc = {background : ' linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)'};
+
+    return ratingBgc;
+  }
+  
   renderBookList();
   initActions();
-  console.log(favoriteBooks);
 }
